36aadfc0242324dc04612a6822be96096d078637
noorksangha <113377440+noorksangha@users.noreply.github.com>
2024-10-10 14:14:07 -0700

    Add files via upload

---
+++ frontend/app/src/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div>
+            <h1>Categories</h1>
+            <p>This is the categories page.</p>
+        </div>
+    );
+}
+
+export default Categories;
